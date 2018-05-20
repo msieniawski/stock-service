4fa25886bc3083e93e8b3ce79fff462922bab47c
Mikołaj Sieniawski <mikolajsieniawskii@gmail.com>
2018-05-20 15:05:11 +0200

    Disable web environment

--- src/main/kotlin/com/stockservice/configuration/RabbitMqConfiguration.kt
+++ src/main/kotlin/com/stockservice/configuration/RabbitMqConfiguration.kt
@@ -1,12 +1,8 @@
 package com.stockservice.configuration
 
-import com.stockservice.ExchangeRatesReceiver
 import org.springframework.amqp.core.Queue
 import org.springframework.amqp.rabbit.connection.ConnectionFactory
 import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.amqp.rabbit.listener.MessageListenerContainer
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
